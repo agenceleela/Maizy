938f041bc3209710c3fa40ae3b3059ecd6d5371b
agenceleela <agenceleela@gmail.com>
2026-08-20 05:18:11 +0200

    Update AnnouncementBar.tsx

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -1,6 +1,6 @@
 export default function AnnouncementBar() {
   return (
-    <div className="bg-maizy-yellow text-maizy-black py-2 text-center text-xs font-medium tracking-wider">
+    <div className="fixed top-0 left-0 right-0 z-[60] bg-maizy-yellow text-maizy-black py-2 text-center text-xs font-medium tracking-wider">
       <p>LIVRAISON OFFERTE DÈS 150 € | EXPÉDITION MONDIALE</p>
     </div>
   );
